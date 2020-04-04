7fc06ed5b2ece1b247521c88ac54ba43bb5bfda9
prsheth <pranayrsheth@gmail.com>
2020-04-03 22:52:48 -0400

    Updated app to add state and manage different story point based on choices picked, added multiple classes to implement this keeping code modularized

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({this.storyTitle, this.choice1, this.choice2}) {
+    storyTitle = storyTitle;
+    choice1 = choice1;
+    choice2 = choice2;
+  }
+}
